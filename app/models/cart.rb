52cd8421c185f906c15abafe06c46992182b28c2
trybchynchuk <tranny268@gmail.com>
2019-05-30 00:45:25 +0300

    AJAX

---
+++ app/models/cart.rb
@@ -0,0 +1,29 @@
+class Cart < ApplicationRecord
+	has_many :line_items, dependent: :destroy
+ 
+  #...
+ 
+  def add_product(product)
+    current_item = line_items.find_by(product_id: product.id)
+    if current_item
+      current_item.quantity += 1
+    else
+      current_item = line_items.build(product_id: product.id)
+    end
+    current_item
+  end
+
+  def total_price
+    line_items.to_a.sum { |item| item.total_price }
+  end
+ 
+  private
+ 
+    # ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add(:base, 'Line Items present')
+        throw :abort
+      end
+    end
+end
